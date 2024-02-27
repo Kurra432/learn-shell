619f2751d4a8129367e5f25ceb7b411857b6333a
VEERA VENKATESWARARA <kurravenkatesh432@gmail.com>
2024-02-27 10:05:07 +0530

    Variables

--- variable.sh
+++ variable.sh
@@ -5,3 +5,18 @@ name=DevOps
 
 echo a = $a
 echo name = ${name}
+
+#
+#date=26-02-2024
+#by using command substitution
+
+Date=$(date +%F)
+
+echo Today date is ${Date}
+
+#by using Arithmetic
+
+ARTH=$((2+3-1))
+
+echo ARTH = ${ARTH}
+
