6d7f2b172258c11bd596f959dab447dee93f6a48
VEERA VENKATESWARARA <kurravenkatesh432@gmail.com>
2024-02-29 10:23:15 +0530

    Functions

--- functions.sh
+++ functions.sh
@@ -8,3 +8,19 @@ function_name() {
 }
 
 function_name
+
+## we can send inputs to the function and we can access with  Special Variables $1-$n,$*,$#
+
+func_name1() {
+
+echo first Argument = $1
+
+echo first Argument = $2
+
+echo All Arguments = $*
+
+echo No of Arguments = $#
+
+}
+
+func_name1 123 xyz
